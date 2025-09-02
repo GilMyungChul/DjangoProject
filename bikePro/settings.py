61bccc0b8ff2af45732ac0742f286449412168e7
GilMyungChul <audcjf4364@naver.com>
2025-09-02 13:29:17 +0900

    배포용

--- bikePro/settings.py
+++ bikePro/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-l0lu-k%!6tysd+zzy*b7k)ls40hw8oqhm(dn%wv932+$j$psx4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['GilMyungChul.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
